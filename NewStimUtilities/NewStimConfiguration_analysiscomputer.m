fdb11564bb3ea4e6d083ae126a741690d04cd7f9
Alexander Heimel <alexander.heimel@gmail.com>
2025-03-19 10:17:27 +0100

    Cleaned up Configuration folder

--- NewStimUtilities/NewStimConfiguration_analysiscomputer.m
+++ NewStimUtilities/NewStimConfiguration_analysiscomputer.m
@@ -42,13 +42,13 @@ Remote_Comm_port = 1205;
 % this section is important
 % if this computer IS the remote computer, then this section is unimportant
 
-Remote_Comm_remotearchitecture = 'unix'; % options are 'PC', 'Mac' (MacOS9), or 'unix' (Linux, MacOSX)
+Remote_Comm_remotearchitecture = 'PC'; % options are 'PC', 'Mac' (MacOS9), or 'unix' (Linux, MacOSX)
 % the computer type of the remote machine you are talking to (not of THIS computer, necessarily)
 
-Remote_Comm_localprefix = 'C:\Data'; % for example, 'Z:', 'z:', '/Users/Shared/myexperimentdir'
+Remote_Comm_localprefix = '\\vs03.herseninstituut.knaw.nl\VS03-CSF-1\Temp'; 
 % the prefix to the shared directory on THIS computer
 
-Remote_Comm_remoteprefix = '/mnt/THISHOSTNAME/data';
+Remote_Comm_remoteprefix = '\\vs03.herseninstituut.knaw.nl\VS03-CSF-1\Temp';
 % the prefix to the same directory as viewed by the OTHER computer
 
 % Example, suppose your "master" computer is a PC running Windows, and you
@@ -78,29 +78,24 @@ MonitorWindowGlobals;
 MonitorWindowMonitor = 0;  % use the given monitor, 0 is first
 MonitorComputer = 0;       % does this computer have a monitor window?
 
-
-if StimComputer&&haspsychtbox==2  % set up timing and monitor settings
-    Screen('Preference','SecondsMultiplier',1.0);
-    Screen('Preference','Backgrounding',1); 
-else
-    % set the current monitor dimensions for remote comm
-    StimWindowRefresh = 60;
-    StimWindowDepth = 8;
-    StimWindowRect = [ 0 0 800 600 ];
-end
+% set the current monitor dimensions for remote comm
+StimWindowRefresh = 60;
+StimWindowDepth = 8;
+StimWindowRect = [ 0 0 800 600 ];
 
 % pixels_per_cm of the monitor in use
-pixels_per_cm = 200/9.5;
+pixels_per_cm = 1980/50;
 
-NewStimViewingDistance = 15; %cm, 
+NewStimViewingDistance = 60; %cm, 
 
 NewStimTilt = 0; % degree tilt of screen on monitor
 
-NewStimStimDelay = 6; % delay to give acquisition computers time to start
+NewStimStimDelay = 3; % delay to give acquisition computers time to start
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  Gamma correction settings
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 GammaCorrectionTableGlobals;
 GammaCorrectionEnable = 0;
 LoadGammaCorrectionTable('gct_linear.txt');
@@ -119,28 +114,31 @@ LoadGammaCorrectionTable('gct_linear.txt');
 %            the stimulus number is reported on the parallel port according
 %                to Fitzpatrick lab protocol
 
-% StimSerial, for sending messages over serial port in Mac OS9.
+% StimSerial, for sending messages over serial port 
 % Use portlist = serial('Ports') for a list of ports on your Mac.
 % use system('setserial -bg /dev/ttyS*') on linux
+% on windows they are like COM3, check in device manager
+%
 % Available pins are 'dtr' (pin 4 of DE-9 serial port, or pin 20 of a DB-25
 % port) and 'rts' (pin 7 of DE-9 port, or pin 4 of DB-25 port)
 
 StimSerialGlobals; % see help file for description
-StimSerialSerialPort = 0;          % do you want to enable this feature?
-StimSerialScriptIn = '/dev/ttyS0';       % NewStim flips ScriptOutPin on this port when trial starts
-StimSerialScriptInPin = 'dtr';       % flips this pin when trial starts
-StimSerialScriptOut = '/dev/ttyS0';     % same
-StimSerialScriptOutPin = 'dtr';       %
-StimSerialStimIn = '/dev/ttyS0';  % NewStim flips DTR on this this port when individual stimuli start
-StimSerialStimInPin = 'dtr';  % NewStim flips this pin when individual stimuli start
-StimSerialStimOut = '/dev/ttyS0';% same
-StimSerialStimOutPin = 'dtr';% same
+StimSerialSerialPort = 0;       % do you want to enable this feature?
+StimSerialScriptIn = 'COM3';    % NewStim flips ScriptOutPin on this port when trial starts
+StimSerialScriptInPin = 'dtr';  % flips this pin when trial starts
+StimSerialScriptOut = 'COM3';   % same
+StimSerialScriptOutPin = 'dtr'; %
+StimSerialStimIn = 'COM3';      % NewStim flips DTR on this this port when individual stimuli start
+StimSerialStimInPin = 'dtr';    % NewStim flips this pin when individual stimuli start
+StimSerialStimOut = 'COM3';     % same
+StimSerialStimOutPin = 'dtr';   % same
 
 % Display preferences
-NSUseInitialSerialTrigger = 0;
-NSUseStimSerialTrigger = 0;
+NSUseInitialSerialTrigger = 0; % if true, then StimSerialScriptOutPin is flipped off at start of stimulus script (in DisplayStimScript)
+NSUseInitialSerialContinuous = 0; % if true, then StimSerialScriptOutPin stays off for duration of script
+%NSUseStimSerialTrigger = 0; % does not seem to be implemented
 
-StimDisplayOrderRemote = 0;
+StimDisplayOrderRemote = 0; % if true, stimulus computer listens on parallel port for the stimulus number
 StimTriggerClear
 
 %fitzTrigParams.triggerStimOnset = 1; % 0 means trigger BGpre instead of stim onset
